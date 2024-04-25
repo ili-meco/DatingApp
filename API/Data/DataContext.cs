788d48ff9f221ab0b260f9a20cc15e588979787d
Iliana Meco <iliana.meco@hotmail.com>
2024-04-24 22:43:39 -0400

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data; //to call in  different file
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
